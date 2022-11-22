6d9c056e6c44009128373ac0fb7839e7995c6b3e
Tim Fischer <fischeti@iis.ee.ethz.ch>
2022-11-22 17:23:56 +0100

    matmul: Add parallelized version of dense dense matmul

--- sw/dphpc/src/main_matmul_dense_dense.c
+++ sw/dphpc/src/main_matmul_dense_dense.c
@@ -17,6 +17,9 @@ dense_matrix *matrix_A, *matrix_B, *matrix_res;
 
 int main() {
 
+  const int compute_id = snrt_cluster_compute_core_idx();
+  const int compute_num = snrt_cluster_compute_core_num();
+
   if (snrt_is_dm_core()) {
     // Allocate memory for matrices struct
     matrix_A = snrt_l1alloc(sizeof(dense_matrix));
@@ -47,25 +50,48 @@ int main() {
   // Wait for all cores to finish DMA
   snrt_cluster_hw_barrier();
 
-  if (snrt_cluster_compute_core_idx() >= NUM_COMP_CORES || snrt_is_dm_core()) return 0;
+  if (snrt_cluster_compute_core_idx() >= NUM_COMP_CORES || snrt_is_dm_core()) {
+    snrt_cluster_hw_barrier();
+    return 0;
+  }
 
   // Run the matrix multiplication
-  benchmark_get_cycle();
-  matmul_dense_dense(matrix_A, matrix_B, matrix_res);
-  benchmark_get_cycle();
+  if (NUM_COMP_CORES == 1) {
+    benchmark_get_cycle();
+    matmul_dense_dense(matrix_A, matrix_B, matrix_res);
+    benchmark_get_cycle();
+  } else {
+    benchmark_get_cycle();
+    // Create a new matrix struct for each core with
+    // a subset of the matrix data
+    dense_matrix matrix_A_parallel = *matrix_A;
+    dense_matrix matrix_res_parallel = *matrix_res;
+    // Divide the rows of the matrix by the number of cores
+    matrix_A_parallel.rows = matrix_A->rows / NUM_COMP_CORES;
+    // Add an offset to the data pointers
+    matrix_A_parallel.values = matrix_A->values + compute_id * matrix_A_parallel.rows * matrix_A->cols;
+    matrix_res_parallel.values = matrix_res->values + compute_id * matrix_A_parallel.rows * matrix_B->cols;
+    // Run the matrix multiplication with the subset of the data
+    matmul_dense_dense(&matrix_A_parallel, matrix_B, &matrix_res_parallel);
+    benchmark_get_cycle();
+  }
+
+  // Wait for all cores to finish
+  snrt_cluster_hw_barrier();
 
   // Check the result
-  int errors = 0;
-  for (unsigned int i = 0; i < (int)sizeof(C_data_dense)/sizeof(double); i++) {
+  if (compute_id == 0) {
+    int errors = 0;
+    for (unsigned int i = 0; i < (int)sizeof(C_data_dense)/sizeof(double); i++) {
 
-    if (fabs(matrix_res->values[i] - C_data_dense[i]) > 0.001) {
-      errors++;
+      if (fabs(matrix_res->values[i] - C_data_dense[i]) > 0.001) {
+        errors++;
+      }
     }
-  }
 
-  if (errors != 0) {
-    printf("Errors: %d/%d!\n", errors, sizeof(C_data_dense)/sizeof(double));
+    if (errors != 0) {
+      printf("Errors: %d/%d!\n", errors, sizeof(C_data_dense)/sizeof(double));
+    }
+    return 0;
   }
-
-  return 0;
 }
